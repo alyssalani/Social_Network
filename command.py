ec0faef596a56ab0ecc4009a5f852dcf35a86026
alyssalani <alyssalani03@gmail.com>
2023-03-02 11:59:49 -0700

    add showFollowing and makePost

--- command.py
+++ command.py
@@ -63,3 +63,75 @@ elif sys.argv[1] == 'follow':
 
 	connection.commit()
 	connection.close()
+
+elif sys.argv[1] == 'likePost':
+	connection = sqlite3.connect("Social_Network.db")
+	cursor = connection.cursor()
+
+
+	user_id = sys.argv[2]
+	post_id = sys.argv[3]
+
+	cursor.execute("""
+	UPDATE posts
+	SET likes = likes + 1
+	WHERE user_id = ? AND post_id = ?
+	""", (user_id, post_id))
+
+	if cursor.rowcount == 0:
+		print("Invalid user ID or post ID.")
+	else:
+		print("Post liked successfully.")
+
+	connection.commit()
+	connection.close()
+
+elif sys.argv[1] == 'makePost':
+	connection = sqlite3.connect("Social_Network.db")
+	cursor = connection.cursor()
+
+	user_id = sys.argv[2]
+	text = sys.argv[3]
+
+	cursor.execute("""
+	INSERT OR REPLACE INTO posts (user_id, text) 
+	VALUES (?,?)
+	""", (user_id, text))
+
+	if cursor.rowcount == 0:
+	   print("Invalid.")
+	else:
+		print("Posted successfully.")
+
+	connection.commit()
+	connection.close()
+
+elif sys.argv[1] == 'showFollowing':
+	connection = sqlite3.connect("Social_Network.db")
+	cursor = connection.cursor()
+
+	id1 = sys.argv[2];
+
+	cursor.execute("""
+	SELECT count(*)
+	FROM follows JOIN users ON followed_id = user_id
+	WHERE follower_id = ?
+	""", id1)
+
+	print(cursor.fetchone()[0])
+
+	cursor.execute("""
+	SELECT name 
+	FROM follows JOIN users ON followed_id = user_id
+	WHERE follower_id = ?
+	""", id1)
+
+	userList = cursor.fetchall()
+
+	for x in userList:
+	print(x[0])
+
+	connection.commit()
+	connection.close()
+
+elif sys.argv[1] == '
